23dbaef03ece29e0e85042855639213cdc8b7da8
janus_wel <janus.wel.3@gmail.com>
2015-01-12 02:47:42 +0900

    Add directive to calculate password strength

--- input-supporters.js
+++ input-supporters.js
@@ -34,4 +34,58 @@
             }
         };
     });
+
+    var lengthCriteria = 16;
+    var characterKinds = [/[a-z]/g, /[A-Z]/g, /[0-9]/g];
+    var evaluaters = [
+        // length check
+        function (password) {
+            var length = password.length;
+            if (length > lengthCriteria) {
+                return 1;
+            }
+            var theta = (Math.PI * length) / (2 * lengthCriteria);
+            // use squared sin to get strength
+            return Math.pow(Math.sin(theta), 2);
+        },
+        // a number of character kinds check
+        function (password) {
+            var replaced = '';
+            var numberOfKinds = 0;
+            characterKinds.forEach(function (kind) {
+                replaced = password.replace(kind, '', 'g');
+                if (replaced.length != password.length) {
+                    ++numberOfKinds;
+                }
+                password = replaced;
+            });
+            if (password.length > 0) {
+                ++numberOfKinds;
+            }
+            return Math.pow(numberOfKinds / (characterKinds.length + 1), 3);
+        }
+    ];
+
+    module.directive('passwordStrength', function () {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function ($scope, element, attrs, modelController) {
+                var propertyName = attrs.passwordStrength;
+                if (!propertyName) {
+                    throw new Error('Specify property name to store password strength by "password-strength" attr');
+                }
+
+                $scope.$watch(function () {
+                    return modelController.$viewValue;
+                }, function (newValue) {
+                    $scope[propertyName] = evaluaters.map(function (evaluater) {
+                        return evaluater(newValue ? newValue : '');
+                    }).reduce(function (prev, current) {
+                        return prev + current;
+                    }) / evaluaters.length;
+                });
+            }
+        };
+    });
 })();
